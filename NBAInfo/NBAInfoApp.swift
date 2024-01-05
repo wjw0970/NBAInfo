41089b48fa24467013a5a71fe6d99d402356ad16
jiawen wang <wjw0970@gmail.com>
2024-01-06 00:35:11 +0800

    Initial Commit

---
+++ NBAInfo/NBAInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBAInfoApp.swift
+//  NBAInfo
+//
+//  Created by Jiawen Wang on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct NBAInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
